e61ed76b42a2b5fa2e35411ec92e9f7a9f626637
njacob1001 <njacob1001@gmail.com>
2021-04-10 21:18:21 -0500

    expose databae port in a safeport

--- kit/identifier/identifier.go
+++ kit/identifier/identifier.go
@@ -7,7 +7,6 @@ import (
 )
 
 var ErrInvalidClientUUID = errors.New("invalid Client UUID")
-var ErrCreatingClientUUID = errors.New("can't create UUID")
 
 type Identifier struct {
 	String string
@@ -22,13 +21,4 @@ func ValidateIdentifier(value string) (Identifier, error) {
 	return Identifier{
 		String: v.String(),
 	}, nil
-}
-
-func NewIdentifier() Identifier {
-	value := uuid.New()
-
-	return Identifier{
-		String: value.String(),
-	}
-
 }
